b9f6584c82dbb106748b080b8c1bf6998ad19cad
iguessfive <iguessfive@outlook.com>
2025-08-13 20:34:42 +0100

    fruit-slash-0.0.3

--- gameplay_components/fruit_slash/fruit.gd
+++ gameplay_components/fruit_slash/fruit.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 @export var texture: Texture2D = preload("res://art/fruits/Apple.png")
-@export var slash_force: float = 800.0
-@export var gravity: float = 100.0
+@export var slash_force: float = 300.0
+@export var gravity: float = 250.0
 
 var speed: float = 100.0
 var move_direction: Vector2 = Vector2.RIGHT
@@ -12,11 +12,15 @@ var move_direction: Vector2 = Vector2.RIGHT
 func _ready() -> void:
 	sprite.texture = texture
 
+@warning_ignore("unused_parameter")
+func _process(delta: float) -> void:
+	%H1Sprite.global_rotation = 0.0
+	%H2Sprite.global_rotation = 0.0
+
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# velocity = move_direction * speed
 	move_and_slide()
 
 func slash_fruit(slash_direction: Vector2) -> void:
@@ -25,10 +29,11 @@ func slash_fruit(slash_direction: Vector2) -> void:
 	%CollisionShape2D.set_deferred("disabled", true)
 
 	%SlashedCase.rotation = slash_direction.angle()
-	add_sprites_to_slashed_case()
 
 	%Half1.velocity = slash_direction * slash_force
 	%Half2.velocity = -slash_direction * slash_force
+	%Half1.gravity = gravity
+	%Half2.gravity = gravity
 
 func add_sprites_to_slashed_case() -> void:
 	var half_sprite_1 = sprite.duplicate()
--- gameplay_components/fruit_slash/slasher_cursor.gd
+++ gameplay_components/fruit_slash/slasher_cursor.gd
@@ -1,30 +1,37 @@
 extends Area2D
 
-var can_register_position := false
-var impact_point := Vector2()
-
 var is_slasher_active := false
 var click_point := Vector2()
+var impact_point := Vector2()
+var max_trail_points := 5
 
-func _process(_delta: float) -> void:
+@onready var slash_line: Line2D = %Line2D
+
+@warning_ignore("unused_parameter")
+func _process(delta: float) -> void:
 	global_position = get_global_mouse_position()
 
-	if Input.is_action_pressed("left_click") and not is_slasher_active:
+	if Input.is_action_just_pressed("left_click"):
 		is_slasher_active = true
-		click_point = get_global_mouse_position()
+		slash_line.visible = is_slasher_active
+		click_point = global_position
+		slash_line.add_point(click_point)
 
-	if Input.is_action_just_released("left_click") and is_slasher_active:
+	if Input.is_action_just_released("left_click"):
 		is_slasher_active = false
+		slash_line.visible = is_slasher_active
+		slash_line.clear_points()
 
-	if Input.is_action_pressed("left_click") and can_register_position:
-		impact_point = get_global_mouse_position()
-		can_register_position = false
+	if Input.is_action_pressed("left_click") and is_slasher_active:
+		slash_line.add_point(get_global_mouse_position()) # Updating slash line while dragging
+
+		while slash_line.get_point_count() > max_trail_points:
+			slash_line.remove_point(0)
 
 func _on_body_entered(body:Node2D) -> void:
-	if body.is_in_group("fruits"):
-		can_register_position = true
-		if is_slasher_active:
-			body.slash_fruit(get_slash_normal_direction())
+	impact_point = get_global_mouse_position()
+	if body.is_in_group("fruits") and is_slasher_active:
+		body.slash_fruit(get_slash_direction())
 
-func get_slash_normal_direction() -> Vector2:
-	return (impact_point - click_point).normalized().orthogonal()
+func get_slash_direction() -> Vector2:
+	return (impact_point - click_point).normalized()
